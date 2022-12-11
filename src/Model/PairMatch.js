b724105389ff83ec27525a90696f71488f97617e
cruelladevil <dev.timetravel@gmail.com>
2022-12-11 17:30:24 +0900

    feat: shuffle crews

--- src/Model/PairMatch.js
+++ src/Model/PairMatch.js
@@ -6,6 +6,14 @@ class PairMatch {
   createPairMatch(course, crewNames) {
     this.#crews = crewNames.map((name) => new Crew(course, name));
   }
+
+  shuffleCrews() {
+    const indices = Array.from({ length: this.#crews.length }, (_, i) => i);
+    const suffledIndices = Random.shuffle(indices);
+    const suffledCrews = suffledIndices.map((index) => this.#crews[index]);
+
+    this.#crews = suffledCrews;
+  }
 }
 
 module.exports = PairMatch;
