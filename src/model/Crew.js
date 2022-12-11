6365a487c2fb249251db03afd4677909ea8ac60d
yeongi <cbg5255@gmail.com>
2022-12-11 16:25:24 +0900

    feat : Crew 매칭 이력 저장 기능 구현

--- src/model/Crew.js
+++ src/model/Crew.js
@@ -1,5 +1,14 @@
+const { LEVEL1, LEVEL2, LEVEL3, LEVEL4, LEVEL5 } = require("../Level");
+
 class Crew {
   #course;
+  #matched = {
+    [LEVEL1]: [],
+    [LEVEL2]: [],
+    [LEVEL3]: [],
+    [LEVEL4]: [],
+    [LEVEL5]: [],
+  };
   #name;
 
   constructor(name, course) {
@@ -9,6 +18,14 @@ class Crew {
 
   getName() {
     return this.#name;
+  }
+
+  matching(level, name) {
+    this.#matched[level].push(name);
+  }
+
+  getMatchedRecord(level) {
+    return this.#matched[level];
   }
 }
 
--- src/model/PareMatching.js
+++ src/model/PareMatching.js
@@ -1,5 +1,6 @@
 const { FRONTEND, BACKEND } = require("../Course");
 const { shuffle } = require("../lib/RandomShuffle");
+const Level = require("../Level");
 
 class PareMatching {
   #frontendCrews;
@@ -15,6 +16,31 @@ class PareMatching {
     if (course === FRONTEND) this.#frontendCrews = shuffle(this.#frontendCrews);
     if (course === BACKEND) this.#backendCrews = shuffle(this.#backendCrews);
     return this;
+  }
+
+  frontMatching(level) {
+    let index = 0;
+    while (index < this.#frontendCrews.length - 1) {
+      const currentCrew = this.#frontendCrews[index];
+      const nextCrew = this.#frontendCrews[index + 1];
+      if (!hasMatchedThisLevel(currentCrew, nextCrew, level))
+        matchingThisLevel(currentCrew, nextCrew, level);
+      if (hasMatchedThisLevel(currentCrew, nextCrew, level)) {
+      }
+      count += 2;
+    }
+  }
+
+  hasMatchedThisLevel(cur, next, level) {
+    const history = cur.getMatchedRecord(level);
+    if (history.include(next.getName())) return true;
+    if (!history.include(next.getName())) return false;
+  }
+
+  matchingThisLevel(cur, next, level) {
+    cur.matching(next.getName(), level);
+    next.matching(cur.getName(), level);
+    this.#pare[cur.getName()] = next.getName();
   }
 }
 
