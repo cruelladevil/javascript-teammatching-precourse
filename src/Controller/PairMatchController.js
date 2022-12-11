19eef28e8eb44a5621fa04b2e2e972f61ca37fc0
cruelladevil <dev.timetravel@gmail.com>
2022-12-11 16:41:08 +0900

    feat: switch feature

--- src/Controller/PairMatchController.js
+++ src/Controller/PairMatchController.js
@@ -6,8 +6,33 @@ const PairMatchController = {
   },
 
   readFeature() {
-    InputView.readFeature((input) => {});
+    InputView.readFeature((input) => {
+      switch (input) {
+        case '1':
+          return PairMatchController.createPairMatch();
+        case '2':
+          return PairMatchController.searchPairMatch();
+        case '3':
+          return PairMatchController.initializePairMatch();
+        case 'Q':
+          return PairMatchController.end();
+      }
+    });
   },
+
+  createPairMatch() {
+    console.log('페어 매칭');
+  },
+
+  searchPairMatch() {
+    console.log('페어 조회');
+  },
+
+  initializePairMatch() {
+    console.log('페어 초기화');
+  },
+
+  end() {},
 };
 
 module.exports = PairMatchController;
