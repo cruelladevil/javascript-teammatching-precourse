434ddc391bdf7c8ca370c79ea5322990db789cdf
yeongi <cbg5255@gmail.com>
2022-12-11 15:20:22 +0900

    feat : Course enum class 기능 구현

--- src/model/Course.js
+++ src/model/Course.js
@@ -1,4 +1,15 @@
+const Level = require("./Level");
+
 const Course = {
   FRONTEND: "프론트엔드",
   BACKEND: "백엔드",
+  MISSION: {
+    [Level.LEVEL1]: ["자동차경주", "로또", "숫자야구게임"],
+    [Level.LEVEL2]: ["장바구니", "결제", "지하철노선도"],
+    [Level.LEVEL3]: [],
+    [Level.LEVEL4]: ["성능개선", "배포"],
+    [Level.LEVEL5]: [],
+  },
 };
+
+module.exports = Course;
