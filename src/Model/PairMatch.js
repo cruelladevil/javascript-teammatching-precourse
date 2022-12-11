812572f4501741525494ad3e89e0c117efda6afd
cruelladevil <dev.timetravel@gmail.com>
2022-12-11 17:40:49 +0900

    feat: shuffle crews and create pairs

--- src/Model/PairMatch.js
+++ src/Model/PairMatch.js
@@ -1,18 +1,36 @@
+const { Random } = require('@woowacourse/mission-utils');
 const Crew = require('./Crew');
 
 class PairMatch {
   #crews = [];
+  #pairs = [];
 
   createPairMatch(course, crewNames) {
     this.#crews = crewNames.map((name) => new Crew(course, name));
+    this.shuffleCrews();
+    this.createPairs();
   }
 
   shuffleCrews() {
     const indices = Array.from({ length: this.#crews.length }, (_, i) => i);
-    const suffledIndices = Random.shuffle(indices);
-    const suffledCrews = suffledIndices.map((index) => this.#crews[index]);
+    const shuffledIndices = Random.shuffle(indices);
+    const shuffledCrews = shuffledIndices.map((index) => this.#crews[index]);
 
-    this.#crews = suffledCrews;
+    this.#crews = shuffledCrews;
+  }
+
+  createPairs() {
+    for (let i = 0; i < this.#crews.length - 1; i += 2) {
+      this.#pairs.push(this.#crews.slice(i, i + 2));
+    }
+
+    if (this.#crews.length % 2 === 1) this.#pushOddPair();
+  }
+
+  #pushOddPair() {
+    const lastPair = this.#pairs[this.#pairs.length - 1];
+    const lastCrew = this.#crews[this.#crews.length - 1];
+    lastPair.push(lastCrew);
   }
 }
 
