eed1f77114e732a1e8f095febfac560f7f6fc6e8
yeongi <cbg5255@gmail.com>
2022-12-11 18:09:21 +0900

    feat : 매칭 컨트롤러 기능 구현

--- src/controller/MatchingController.js
+++ src/controller/MatchingController.js
@@ -1,20 +1,95 @@
-const { LEVEL1 } = require("../Level");
+const { FRONTEND, BACKEND, MISSION } = require("../Course");
 const PareMatching = require("../model/PareMatching");
+const {
+  inputFunction,
+  inputCourseMission,
+  inputRematching,
+} = require("../view/InputView");
+const {
+  printError,
+  printResult,
+  printCourseMission,
+  printClose,
+} = require("../view/OutputView");
 const CourseCrewController = require("./CourseCrewController");
+const ViewController = require("./ViewController");
 
 class MatchingController {
   #pareMatching;
+  #missionPare;
 
   constructor() {
     const frontCrews = CourseCrewController.initBackCrew();
     const backCrews = CourseCrewController.initFrontCrew();
     this.#pareMatching = new PareMatching(frontCrews, backCrews);
-    //입력 받기
   }
 
   start() {
-    const pare = this.#pareMatching.frontMatching(LEVEL1).getMyPare();
-    console.log(pare);
+    inputFunction((command) => {
+      try {
+        // validator(cammand);
+      } catch (error) {
+        printError(error);
+        this.start();
+      }
+      this.selectFuction(command);
+    });
+  }
+
+  selectFuction(command) {
+    //to do 나중에 고치기
+    if (command === "1") this.selectCourseMission();
+    if (command === "2") this.selectCourseMission();
+    if (command === "3") this.selectCourseMission();
+    if (command === "Q") this.endMatching();
+  }
+
+  selectCourseMission() {
+    printCourseMission(`${FRONTEND},${BACKEND}`, ViewController.missionView());
+    inputCourseMission((answer) => {
+      try {
+        // validator(cammand);
+      } catch (error) {
+        printError(error);
+        this.start();
+      }
+      this.matchingStart(answer);
+    });
+  }
+
+  matchingStart(answer) {
+    const [course, level, mission] = answer.split(" ,");
+    const pare = this.getMatchingResult(course, level, mission);
+    this.matchingPrint(pare);
+  }
+
+  getMatchingResult(course, level) {
+    if (course === FRONTEND) this.#pareMatching.frontMatching(level);
+    if (course === BACKEND) this.#pareMatching.backMatching(level);
+    // mission 에 매칭 이력 넣어야 한다.
+    return this.#pareMatching.getMyPare();
+  }
+
+  matchingPrint(pare) {
+    printResult(ViewController.pareView(pare));
+    this.start();
+  }
+
+  selectRematching() {
+    inputRematching((command) => {
+      try {
+        // validator(cammand);
+      } catch (error) {
+        printError(error);
+        this.selectRematching();
+      }
+      this.selectFuction(command);
+    });
+  }
+
+  endMatching() {
+    printClose();
+    return;
   }
 }
 
